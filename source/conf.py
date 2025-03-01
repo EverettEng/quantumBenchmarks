cb7cf1044fe1be5254b8c21501bf851fa1091f2c
EverettEng <everetteng07@gmail.com>
2025-03-01 18:16:50 -0500

    trying to get right hteme

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,7 @@ author = 'Everett Eng'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx_rtd_theme"]
 
 html_theme = "sphinx_rtd_theme"
 
